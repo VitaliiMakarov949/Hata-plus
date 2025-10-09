425f82e11c6da115b97bae4e0a1a2e5240101c3d
VitaliiMakarov949 <vitaliimakarov854@gmail.com>
2025-10-09 18:39:13 +0300

    1

--- js/script.js
+++ js/script.js
@@ -19,31 +19,31 @@ black_bg.onclick = () => {
     black_bg.style.display = "block";
     document.body.classList.remove('menu-open');
 }
-eye.onclick = () => {
-    if(eye.checked) {
-        pass.setAttribute("type", "text")
-    } else {
-        pass.setAttribute("type", "password")
-    }
-}
+// eye.onclick = () => {
+//     if(eye.checked) {
+//         pass.setAttribute("type", "text")
+//     } else {
+//         pass.setAttribute("type", "password")
+//     }
+// }
 const button = document.getElementById('button');
 const popup = document.getElementById('popup');
 const dark = document.getElementById('dark');
 const close = document.getElementById('close');
 
 button.onclick = () => {
-    popup.style.right = "0"
     popup.style.display = "block"
+    dark.style.display = "block"
 }
 dark.onclick = () => {
-    popup.style.right = "-100%";
-    dark.style.display = "block";
+    dark.style.display = "block"
 }
 
 close.onclick = () => {
-    close.style.right = "-100%"
-    close.style.display = "block"
+    popup.style.display = "none"
+    dark.style.display = "none"
 }
+
 
 
 
