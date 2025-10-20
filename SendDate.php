3a339891115a0e16bdc0bcab9052c223dc98bada
VitaliiMakarov949 <vitaliimakarov854@gmail.com>
2025-10-20 11:18:54 +0300

    1

--- SendDate.php
+++ SendDate.php
@@ -8,12 +8,14 @@ try {
 $token = "8232410439:AAHrDBMkmIxiK6SNi9s-PFiKqPXtwAi3s3k";
 $chat_id = 7554874104;
 
-$name = urlencode("name");
-$comment = urlencode("$comment");
-$date = urlencode("$date");
+$textMessage = "<b>Нова заявка з сайту</b>%0A"
+             . "<b>Ім’я:</b> $name%0A"
+             . "<b>Телефон:</b> $phone%0A"
+             . "<b>Дата:</b> $date";
 
 $textMessage = urlencode ("$textMessage");
 $urlQuery = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$textMessage<b>Нова заявка на сайті</b>Name:$name%OADate:$date%OAComment:$comment";
 $urlQuery .= "&parse_mode=HTML";
 $result = file_get_contents($urlQuery);
+
 ?>
